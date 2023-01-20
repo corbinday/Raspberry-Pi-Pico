bdc3b07414e55f33b7a24681e5b932177a0181f6
Corbin Day <raizingthebar@gmail.com>
2023-01-20 14:23:37 +0000

    add new script for conrolling unicorn display

--- message-board.py
+++ message-board.py
@@ -1,11 +1,14 @@
 import mb3x3
 import mb5x5
+import unicorn7x17
 import color
 
 INTERVAL = 0.09
+
+SCREEN = unicorn7x17
 
 # the message to print
 message = "Merry Christmas"
 
 while True:
-    mb3x3.display(message, INTERVAL)
+    SCREEN.display(message, INTERVAL)
